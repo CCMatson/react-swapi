3d2a8c347c29dbd2bb6d93d2ffae37eeb0e00b4f
CCMatson <catherine.matson@gmail.com>
2023-01-26 15:59:28 -0800

    add nav component

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <header className='nav'>
+      {/* <img
+        src='/images/logo512.gif'
+        style={{ width: "100px", height: "100px" }}
+        className="App-logo"
+        alt="logo"
+      /> */}
+			<h1>Niki Minaj's StarShips</h1>
+      {/* <Link to="/shop">Shop</Link> */}
+  
+    </header>
+  );
+};
+
+export default NavBar;
